2e6a74aa1fc149198f423f3ded076cd47dc6f83e
VijayKumarKTG <vijaykumarktg18@gmail.com>
2020-08-16 16:28:23 +0630

    Add snapshot test using react-test-renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,70 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM, { render } from 'react-dom';
+import renderer from 'react-test-renderer';
 import { App, Todo, Alert } from './App';
+
+describe('App', () => {
+  it('renders App without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has valid snapshot', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Todo', () => {
+  let props = {
+    title: 'Stroy Telling',
+    desc: 'Tell someone a story',
+    completed: true,
+    index: 0,
+  };
+
+  it('renders Todo without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Todo {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has valid snapshot', () => {
+    const component = renderer.create(<Todo {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Alert', () => {
+  it('render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Alert>
+        <p className='alert__title'>Please Enter your name!</p>
+        <form>
+          <input type='text' className='alert__input' placeholder='Name' />
+          <button className='alert__button'>Add</button>
+        </form>
+      </Alert>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has valid snapshot', () => {
+    const component = renderer.create(
+      <Alert>
+        <p className='alert__title'>Please Enter your name!</p>
+        <form>
+          <input type='text' className='alert__input' placeholder='Name' />
+          <button className='alert__button'>Add</button>
+        </form>
+      </Alert>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
